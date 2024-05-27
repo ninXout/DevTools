5c92bb29b10df012e1d6f35eb1b4b54ae87f3a3d
shloakmenon <58670749+ninXout@users.noreply.github.com>
2024-05-27 11:39:39 -0400

    Fix MacOS text input and some warnings

--- src/platform/platform.hpp
+++ src/platform/platform.hpp
@@ -2,6 +2,9 @@
 
 #include <imgui.h>
 #include <imgui_internal.h>
+#ifdef GEODE_IS_MACOS
+    #include <OpenGL/gl.h>
+#endif
 
 ImRect& getGDWindowRect();
 bool& shouldPassEventsToGDButTransformed();
